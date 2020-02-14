18d0dc7fedc4544b110bc513cd98e263d18e0da1
David Medina <davidmpuyol@gmail.com>
2020-02-14 22:30:59 +0100

    .

---
+++ precache-manifest.1c270ce0f366923d0c6e3dc56bb0345d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebf3124ccb796d6f779",
+    "url": "css/app.bc0a123c.css"
+  },
+  {
+    "revision": "74d290350d34e05883b8",
+    "url": "css/chunk-vendors.14e89850.css"
+  },
+  {
+    "revision": "efcf697f60d76fa4d66da071f7978c5f",
+    "url": "img/checked.efcf697f.png"
+  },
+  {
+    "revision": "f5fe47d10d48d31279377bcf6a0ad50a",
+    "url": "img/delete-icon.f5fe47d1.png"
+  },
+  {
+    "revision": "2a9dc306d8f3aec4ad75a243252fbb34",
+    "url": "img/unchecked.2a9dc306.png"
+  },
+  {
+    "revision": "b9a136aed3b51a18693e8b00e1b1a83a",
+    "url": "index.html"
+  },
+  {
+    "revision": "cebf3124ccb796d6f779",
+    "url": "js/app.845977e2.js"
+  },
+  {
+    "revision": "74d290350d34e05883b8",
+    "url": "js/chunk-vendors.616b5b3d.js"
+  },
+  {
+    "revision": "223a1a704810abb28b530a7ca19aa398",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
